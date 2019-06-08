5139976a0e609343ffa0818fa375fac9d79d6717
Trace Larson <trace.larson4917@gmail.com>
2019-06-07 20:29:51 -0500

    created axios call to api for information and display album title in albumList view if data is loaded. show loading...while waiting

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import axios from "axios";
 
 class AlbumList extends Component {
   state = {
-    "": ""
+    data: []
   };
-  componentDidMount() {}
+  async componentDidMount() {
+    const response = await axios.get(
+      "https://rallycoding.herokuapp.com/api/music_albums"
+    );
+    await console.log(response);
+    await this.setState({ data: response.data });
+  }
 
   render() {
-    return (
-      <View>
-        <Text>Albumlist</Text>
-      </View>
-    );
+    const { data } = this.state;
+    let dataDisplay;
+    data.length == 0
+      ? (dataDisplay = <Text>loading ...</Text>)
+      : (dataDisplay = data.map((album, index) => (
+          <Text key={index}>{album.title}</Text>
+        )));
+    return <View>{dataDisplay}</View>;
   }
 }
 
