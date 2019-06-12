3bb283bcc51a6f1bdffb210461dd47324971cb1d
Trace Larson <trace.larson4917@gmail.com>
2019-06-11 22:30:52 -0500

    made button reusable along with button content

--- ios/Albums/AppDelegate.m
+++ ios/Albums/AppDelegate.m
@@ -39,4 +39,14 @@
 #endif
 }
 
+// iOS 9.x or newer
+#import <React/RCTLinkingManager.h>
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
 @end
